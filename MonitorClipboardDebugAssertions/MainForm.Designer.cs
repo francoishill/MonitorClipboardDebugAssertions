c32a280960a484bae6a236477ddcad72b80f3c51
FrancoisLaptopDell <FrancoisLaptopDell@47bff963-4e93-b249-9c78-26e2026f0f90>
2012-12-24 10:21:39 +0000

    Initial commit.
    git-svn-id: svn://localhost/monitorclipboarddebugassertions/trunk@2 47bff963-4e93-b249-9c78-26e2026f0f90

---
+++ MonitorClipboardDebugAssertions/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MonitorClipboardDebugAssertions
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.notifyIconTrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStripTrayIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.changeProcessnameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.changeProcessargumentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.changerootAlbionDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.contextMenuStripTrayIcon.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIconTrayIcon
+			// 
+			this.notifyIconTrayIcon.ContextMenuStrip = this.contextMenuStripTrayIcon;
+			this.notifyIconTrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconTrayIcon.Icon")));
+			this.notifyIconTrayIcon.Text = "Monitoring clipboard for debug assertions";
+			this.notifyIconTrayIcon.Visible = true;
+			// 
+			// contextMenuStripTrayIcon
+			// 
+			this.contextMenuStripTrayIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeProcessnameToolStripMenuItem,
+            this.changeProcessargumentsToolStripMenuItem,
+            this.changerootAlbionDirectoryToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+			this.contextMenuStripTrayIcon.Name = "contextMenuStripTrayIcon";
+			this.contextMenuStripTrayIcon.Size = new System.Drawing.Size(229, 120);
+			// 
+			// changeProcessnameToolStripMenuItem
+			// 
+			this.changeProcessnameToolStripMenuItem.Name = "changeProcessnameToolStripMenuItem";
+			this.changeProcessnameToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+			this.changeProcessnameToolStripMenuItem.Text = "Change process &name";
+			this.changeProcessnameToolStripMenuItem.Click += new System.EventHandler(this.changeProcessnameToolStripMenuItem_Click);
+			// 
+			// changeProcessargumentsToolStripMenuItem
+			// 
+			this.changeProcessargumentsToolStripMenuItem.Name = "changeProcessargumentsToolStripMenuItem";
+			this.changeProcessargumentsToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+			this.changeProcessargumentsToolStripMenuItem.Text = "Change process &arguments";
+			this.changeProcessargumentsToolStripMenuItem.Click += new System.EventHandler(this.changeProcessargumentsToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(225, 6);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+			this.exitToolStripMenuItem.Text = "E&xit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// changerootAlbionDirectoryToolStripMenuItem
+			// 
+			this.changerootAlbionDirectoryToolStripMenuItem.Name = "changerootAlbionDirectoryToolStripMenuItem";
+			this.changerootAlbionDirectoryToolStripMenuItem.Size = new System.Drawing.Size(228, 22);
+			this.changerootAlbionDirectoryToolStripMenuItem.Text = "Change &root Albion directory";
+			this.changerootAlbionDirectoryToolStripMenuItem.Click += new System.EventHandler(this.changerootAlbionDirectoryToolStripMenuItem_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Opacity = 0D;
+			this.ShowInTaskbar = false;
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.Shown += new System.EventHandler(this.Form1_Shown);
+			this.contextMenuStripTrayIcon.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIconTrayIcon;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStripTrayIcon;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem changeProcessnameToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem changeProcessargumentsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripMenuItem changerootAlbionDirectoryToolStripMenuItem;
+	}
+}
+
---
+++ MonitorClipboardDebugAssertions/MainForm.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+
+namespace MonitorClipboardDebugAssertions
+{
+	public partial class MainForm : Form
+	{
+		const string settingsfileProcessname = "processname.fjset";
+		const string settingsfileProcessarguments = "processarguments.fjset";
+		const string settingsfileRootAlbionDirectory = "rootalbionfolder.fjset";
+		//const string rootAlbionFolder = @"C:\devKiln\Albion\";
+
+		public MainForm()
+		{
+			InitializeComponent();
+
+			nextClipboardViewer = SetClipboardViewer(this.Handle);
+			ClipboardChanged += new EventHandler<ClipboardChangedEventArgs>(Form1_ClipboardChanged);
+		}
+
+		private static void ResetCurrentDir()
+		{
+			var path = Environment.GetCommandLineArgs()[0].TrimEnd('\\');
+			Environment.CurrentDirectory = Path.GetDirectoryName(path);
+		}
+
+		public string ProcessNameOrFullpath
+		{
+			get
+			{
+				ResetCurrentDir();
+				if (File.Exists(settingsfileProcessname))
+					return File.ReadAllText(settingsfileProcessname).Trim();
+				else
+				{
+					var tmpstr = SharedClasses.DialogBoxStuff.InputDialog("Please enter the process name or fullpath, ie. notepad, notepad2, notepad++");
+					if (tmpstr != "")
+					{
+						//ProcessNameOrFullpath = tmpstr;
+						File.WriteAllText(settingsfileProcessname, tmpstr);
+						return tmpstr;
+					}
+					else
+					{
+						UserMessages.ShowWarningMessage("No process name entered, will not be able to run");
+						return "";
+					}
+				}
+			}
+			set
+			{
+				try
+				{
+					ResetCurrentDir();
+					File.WriteAllText(settingsfileProcessname, value);
+				}
+				catch (Exception exc)
+				{
+					UserMessages.ShowErrorMessage("Error writing file: " + exc.Message);
+				}
+			}
+		}
+		public string ProcessArguments
+		{
+			get
+			{
+				ResetCurrentDir();
+				if (File.Exists(settingsfileProcessarguments))
+					return File.ReadAllText(settingsfileProcessarguments).Trim();
+				else
+				{
+					var tmpstr = SharedClasses.DialogBoxStuff.InputDialog("Please enter the process arguments with {0} for linenumber and {1} for filepath, ie."
+						+ Environment.NewLine
+						+ "notepad2 = /g {0} \"{1}\""
+						+ Environment.NewLine
+						+ "notepad++ = -n{0}\"{1}\"").Trim();
+					if (tmpstr != "")
+					{
+						//ProcessNameOrFullpath = tmpstr;
+						File.WriteAllText(settingsfileProcessarguments, tmpstr);
+						return tmpstr;
+					}
+					else
+					{
+						UserMessages.ShowWarningMessage("No process arguments entered, will not be able to run correctly.");
+						return "";
+					}
+				}
+			}
+			set
+			{
+				try
+				{
+					ResetCurrentDir();
+					File.WriteAllText(settingsfileProcessarguments, value);
+				}
+				catch (Exception exc)
+				{
+					UserMessages.ShowErrorMessage("Error writing file: " + exc.Message);
+				}
+			}
+		}
+		public string RootAlbionDirectory
+		{
+			get
+			{
+				ResetCurrentDir();
+				if (File.Exists(settingsfileRootAlbionDirectory))
+					return File.ReadAllText(settingsfileRootAlbionDirectory).Trim();
+				else
+				{
+					var tmpstr = SharedClasses.DialogBoxStuff.InputDialog("Please enter the root directory for Albion, the one containing proj folder.").Trim();
+					if (tmpstr != "")
+					{
+						//ProcessNameOrFullpath = tmpstr;
+						if (!tmpstr.EndsWith("\\"))
+							tmpstr += "\\";
+						if (!Directory.Exists(tmpstr))
+							UserMessages.ShowWarningMessage("Directory does not exist, this will cause issues on trying to open assertion source file.");
+						File.WriteAllText(settingsfileRootAlbionDirectory, tmpstr);
+						return tmpstr;
+					}
+					else
+					{
+						UserMessages.ShowWarningMessage("No albion directory entered, this will cause issues on trying to open assertion source file.");
+						return "";
+					}
+				}
+			}
+			set
+			{
+				try
+				{
+					ResetCurrentDir();
+					File.WriteAllText(settingsfileRootAlbionDirectory, value);
+				}
+				catch (Exception exc)
+				{
+					UserMessages.ShowErrorMessage("Error writing file: " + exc.Message);
+				}
+			}
+		}
+
+		void Form1_ClipboardChanged(object sender, MainForm.ClipboardChangedEventArgs e)
+		{
+			var textData = e.DataObject.GetData(DataFormats.Text);
+			if (textData != null)
+			//if (e.DataObject.GetDataPresent(DataFormats.Text))
+			{
+				string fullpath = null;
+				int lineNum = -1;
+
+				string clipboardText = textData.ToString();//e.DataObject.GetData(DataFormats.Text).ToString();
+				if (
+					clipboardText.IndexOf("Microsoft Visual C++ Debug Library", StringComparison.InvariantCultureIgnoreCase) != -1 &&
+					clipboardText.IndexOf("Debug Assertion Failed!", StringComparison.InvariantCultureIgnoreCase) != -1 &&
+					clipboardText.IndexOf("File: ", StringComparison.InvariantCultureIgnoreCase) != -1)
+				{
+					string[] lines = clipboardText.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+					string tmpFileColon = "File: ";
+					string tmpLineColon = "Line: ";
+					for (int i = 0; i < lines.Length; i++)
+						if (lines[i].StartsWith(tmpFileColon, StringComparison.InvariantCultureIgnoreCase))
+						{
+							string relFilepath = lines[i].Substring(tmpFileColon.Length);
+							fullpath = RootAlbionDirectory + relFilepath;//rootAlbionFolder + relFilepath;
+						}
+						else if (lines[i].StartsWith(tmpLineColon, StringComparison.InvariantCultureIgnoreCase))
+						{
+							if (!int.TryParse(lines[i].Substring(tmpLineColon.Length), out lineNum))
+								lineNum = -1;
+						}
+				}
+				else if (
+					clipboardText.IndexOf("AdbAssert", StringComparison.InvariantCultureIgnoreCase) != -1 &&
+					clipboardText.IndexOf("Adb assertion failed: ", StringComparison.InvariantCultureIgnoreCase) != -1)
+				{
+					string[] lines = clipboardText.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+					string tmpFailedColon = "Adb assertion failed: ";
+					for (int i = 0; i < lines.Length; i++)
+						if (lines[i].StartsWith(tmpFailedColon, StringComparison.InvariantCultureIgnoreCase))
+						{
+							int lastColonIndex = lines[i].LastIndexOf(':');
+							string relFilePath = lines[i].Substring(tmpFailedColon.Length, lastColonIndex - tmpFailedColon.Length);
+							if (File.Exists(relFilePath))
+								fullpath = relFilePath;
+							else
+								fullpath = RootAlbionDirectory + relFilePath;
+							if (!int.TryParse(lines[i].Substring(lastColonIndex + 2), out lineNum))
+								lineNum = -1;
+						}
+					//Adb assertion failed: proj\adb\Wrap\ModTable.cpp: 155
+				}
+
+				if (fullpath != null && lineNum != -1)
+				{
+					if (!File.Exists(fullpath))
+						UserMessages.ShowWarningMessage("Cannot find file: " + fullpath);
+					else
+					{
+						try
+						{
+							var proc = Process.Start(
+								ProcessNameOrFullpath,
+								string.Format(ProcessArguments, lineNum, fullpath));
+							//Process.Start("notepad",
+							//string.Format("/g {0} \"{1}\"", lineNum, fullpath));
+							//Process.Start("notepad++",
+							//string.Format("-n{0}\"{1}\"", lineNum, fullpath));
+							Win32Api.SetForegroundWindow(proc.MainWindowHandle);
+						}
+						catch (Exception Exception)
+						{
+							UserMessages.ShowErrorMessage("Error occurred opening file and then SetForegroundWindow: " + Exception.Message);
+						}
+					}
+				}
+				/*
+				 ---------------------------
+				AdbAssert
+				---------------------------
+				Adb assertion failed: proj\adb\Wrap\ModTable.cpp: 155
+				---------------------------
+				OK   
+				--------------------------- 
+				*/
+				/*
+				---------------------------
+				Microsoft Visual C++ Debug Library
+				---------------------------
+				Debug Assertion Failed!
+
+				Program: ...ln\Albion\tundra-output\win32-baked-debug-default\Wadiso6.exe
+				File: proj\adb\Triggers\StdTriggers.cpp
+				Line: 515
+
+				For information on how your program can cause an assertion
+				failure, see the Visual C++ documentation on asserts.
+
+				(Press Retry to debug the application)
+				---------------------------
+				Abort   Retry   Ignore   
+				---------------------------
+				*/
+			}
+		}
+
+		#region Extra stuff
+
+		[DllImport("User32.dll", CharSet = CharSet.Auto)]
+		public static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+		public event EventHandler<ClipboardChangedEventArgs> ClipboardChanged;
+		private IntPtr nextClipboardViewer;
+
+		[DllImport("User32.dll")]
+		protected static extern int SetClipboardViewer(int hWndNewViewer);
+
+		[DllImport("User32.dll", CharSet = CharSet.Auto)]
+		public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+		private void Form1_Shown(object sender, EventArgs e)
+		{
+			this.Hide();
+
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			ChangeClipboardChain(this.Handle, nextClipboardViewer);
+		}
+
+		private void changeProcessnameToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			File.Delete(settingsfileProcessname);
+			var t = ProcessNameOrFullpath;
+		}
+
+		private void changeProcessargumentsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			File.Delete(settingsfileProcessarguments);
+			var t = ProcessArguments;
+		}
+
+		private void changerootAlbionDirectoryToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			File.Delete(settingsfileRootAlbionDirectory);
+			var t = RootAlbionDirectory;
+		}
+
+		protected override void WndProc(ref System.Windows.Forms.Message m)
+		{
+			// defined in winuser.h
+			const int WM_DRAWCLIPBOARD = 0x308;
+			const int WM_CHANGECBCHAIN = 0x030D;
+
+			switch (m.Msg)
+			{
+				case WM_DRAWCLIPBOARD:
+					OnClipboardChanged();
+					SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+					break;
+
+				case WM_CHANGECBCHAIN:
+					if (m.WParam == nextClipboardViewer)
+						nextClipboardViewer = m.LParam;
+					else
+						SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+					break;
+
+				default:
+					base.WndProc(ref m);
+					break;
+			}
+		}
+
+		void OnClipboardChanged()
+		{
+			try
+			{
+				IDataObject iData = Clipboard.GetDataObject();
+				if (ClipboardChanged != null)
+				{
+					ClipboardChanged(this, new ClipboardChangedEventArgs(iData));
+				}
+
+			}
+			catch (Exception e)
+			{
+				// Swallow or pop-up, not sure
+				// Trace.Write(e.ToString());
+				UserMessages.ShowErrorMessage(e.ToString());
+			}
+		}
+
+		public class ClipboardChangedEventArgs : EventArgs
+		{
+			public readonly IDataObject DataObject;
+
+			public ClipboardChangedEventArgs(IDataObject dataObject)
+			{
+				DataObject = dataObject;
+			}
+		}
+
+		#endregion Extra stuff
+	}
+}
